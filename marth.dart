9be52b3d70a0b49091212d921e1d90459c6c1199
tukuchrispus <tukuchrispus@gmail.com>
2023-11-07 12:20:46 +0300

    changes

---
+++ marth.dart
@@ -0,0 +1,9 @@
+import"dart:math";
+void(main){
+  int num1 =140;
+  double num2=30.89;
+  //add the two)
+  print(num1+num2);
+double result=num1+num2;
+print(results);
+}
---
+++ second.dart
@@ -0,0 +1,8 @@
+//we are talking about variable in dart
+/*
+variable can be denoted using the word var
+*/
+void main(){
+  var name ="Ronald Ngal";
+  print(name);
+}
